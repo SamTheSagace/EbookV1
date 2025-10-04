d56cfc3656a882038ee767cf73446ce8fb8c48f4
SamSagace <74056270+SamTheSagace@users.noreply.github.com>
2025-10-04 21:57:52 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/EbookV1/', 
+  base: '/EbookV1/', // 👈 exact repo name (case-sensitive)
 })
